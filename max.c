95192ddb94c3215dc2a95164fcdbb6764def04cb
WilliamNB2003 <williamnobo2003@gmail.com>
2023-10-05 12:47:39 +0200

    y

--- max.c
+++ max.c
@@ -8,8 +8,9 @@ int max(int *numbers, int size) {
   if (size > 0){    /*Checks for preconditions. The array must contain more tha 0 elements*/
         int max = numbers[0];  /*Sets the first element in the array as the largest integer*/
         for (int i = 0; i < size; i++){   /*Goes through all the elements in the array*/
-            if (numbers[i] > max){  /*If an integer is larger than the largest integer so far, then it becomes the new largest integer*/
-                max = numbers[i];
+            int y = numbers[i];
+            if (y > max){  /*If an integer is larger than the largest integer so far, then it becomes the new largest integer*/
+                max = y;
             }
         }
         return max;
